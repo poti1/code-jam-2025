300ccbbe0ee2c471f12a4f142888989a4e5a05a1
Michael Beal <2018mjb@gmail.com>
2025-08-11 15:01:40 -0400

    Move persistent Tokenizer atrrs to ParserState class
    Some of the attributes that were originally in the Tokenizer may need
    to persist when processing characters.
    
    Relevant Issue: #2

--- html_parser.py
+++ html_parser.py
@@ -10,36 +10,6 @@ def parser_error(row: int, col: int, message: str) -> None:
 
 
 @dataclass
-class ParserState:
-    """Represent state of the parser."""
-
-    pause: bool
-    insertion_mode: Literal[
-        "initial",
-        "before html",
-        "before head",
-        "in head",
-        "in head noscript",
-        "after head",
-        "in body",
-        "text",
-        "in table",
-        "in table text",
-        "in caption",
-        "in column group",
-        "in table body",
-        "in row",
-        "in cell",
-        "in template",
-        "after body",
-        "in frameset",
-        "after frameset",
-        "after after body",
-        "after after frameset",
-    ] = "initial"
-
-
-@dataclass
 class Token:
     """Represent HTML tokens."""
 
@@ -89,37 +59,67 @@ def tree_constructor(token: Token) -> None:  # https://html.spec.whatwg.org/mult
     print(token)
 
 
+@dataclass
+class ParserState:
+    """Represent state of the parser."""
+
+    pause: bool
+    token: Token
+    temp_buff: str
+    insertion_mode: Literal[
+        "initial",
+        "before html",
+        "before head",
+        "in head",
+        "in head noscript",
+        "after head",
+        "in body",
+        "text",
+        "in table",
+        "in table text",
+        "in caption",
+        "in column group",
+        "in table body",
+        "in row",
+        "in cell",
+        "in template",
+        "after body",
+        "in frameset",
+        "after frameset",
+        "after after body",
+        "after after frameset",
+    ] = "initial"
+    state: str = "data"
+    # state to return to after being in put in the character reference state
+    return_state = ""
+    need_to_reconsume: bool = False
+
+
 class Tokenizer:
     """HTML Tokenizer."""
 
-    def __init__(self, char: str, pos: tuple[int, int]) -> None:
+    def __init__(self, char: str, pos: tuple[int, int], parser_state: ParserState) -> None:
         self.char = char
         self.col, self.row = pos
-        self.state: str = "data"
-        # state to return to after being in put in the character reference state
-        self.return_state = ""
-        self.token: Token
-        self.temp_buff: str
-        self.need_to_reconsume: bool = False
+        self.parser_state = parser_state
 
     def _emit_token(self, token: Token) -> None:
-        # TODO: The state functions should modify self.token directly
         """Emit a token for `self.char` if the param `char` is not specified."""
         tree_constructor(token)
 
     def _create_token(self, kind: str, **kwargs: str) -> None:
         """Create a token for `self.char` if the param `char` is not specified before emitting it."""
         if "char" not in kwargs:
-            self.token = Token(kind=kind, char=self.char)
+            self.parser_state.token = Token(kind=kind, char=self.char)
         elif "char" in kwargs:
-            self.token = Token(kind=kind, char=kwargs["char"])
+            self.parser_state.token = Token(kind=kind, char=kwargs["char"])
 
     def _switch_to_char_ref_state(
         self,
         return_to: str,
     ) -> None:  # https://html.spec.whatwg.org/multipage/parsing.html#character-reference-state
-        self.return_state = return_to
-        self.state = "character reference"
+        self.parser_state.return_state = return_to
+        self.parser_state.state = "character reference"
 
     # For all the tokenizer state specs:
     # refer to https://html.spec.whatwg.org/multipage/parsing.html#tokenization
@@ -128,7 +128,7 @@ class Tokenizer:
             case "&":
                 self._switch_to_char_ref_state("data")
             case "<":
-                self.state = "tag open"
+                self.parser_state.state = "tag open"
             case "":
                 self._emit_token(Token("EOF", ""))
             case _:
@@ -139,7 +139,7 @@ class Tokenizer:
             case "&":
                 self._switch_to_char_ref_state("rcdata")
             case "<":
-                self.state = "rcdata lt sign"
+                self.parser_state.state = "rcdata lt sign"
             case "":
                 self._emit_token(Token("EOF", ""))
             case _:
@@ -150,7 +150,7 @@ class Tokenizer:
             case "&":
                 self._switch_to_char_ref_state("rawtext")
             case "<":
-                self.state = "rcdata lt sign"
+                self.parser_state.state = "rcdata lt sign"
             case "":
                 self._emit_token(Token("EOF", ""))
             case _:
@@ -159,7 +159,7 @@ class Tokenizer:
     def _script_data_state(self) -> None:
         match self.char:
             case "<":
-                self.state = "script data lt sign"
+                self.parser_state.state = "script data lt sign"
             case "":
                 self._emit_token(Token("EOF", ""))
             case _:
@@ -175,35 +175,35 @@ class Tokenizer:
     def _tag_open_state(self) -> None:
         match self.char:
             case "!":
-                self.state = "markup declaration"
+                self.parser_state.state = "markup declaration"
             case "/":
-                self.state = "end tag open"
+                self.parser_state.state = "end tag open"
             case "?":
                 errormsg: str = "Unexpected question mark instead of tag-name"
                 parser_error(self.row, self.col, errormsg)
-                self.need_to_reconsume = True
-                self.state = "bogus comment"
+                self.parser_state.need_to_reconsume = True
+                self.parser_state.state = "bogus comment"
             case "":
                 self._emit_token(Token("char", char="<"))
                 self._emit_token(Token("EOF", ""))
             case _:
                 if self.char.isalpha():
                     self._create_token("start tag", name="")
-                    self.state = "tag name"
-                    self.need_to_reconsume = True
+                    self.parser_state.state = "tag name"
+                    self.parser_state.need_to_reconsume = True
                 else:
                     errormsg: str = "Invalid first character of tag-name"
                     parser_error(self.row, self.col, errormsg)
                     self._emit_token(Token("char", char="<"))
-                    self.state = "data"
-                    self.need_to_reconsume = True
+                    self.parser_state.state = "data"
+                    self.parser_state.need_to_reconsume = True
 
     def _end_tag_open_state(self) -> None:
         match self.char:
             case ">":
                 errormsg: str = "Missing end tag name"
                 parser_error(self.row, self.col, errormsg)
-                self.state = "data"
+                self.parser_state.state = "data"
             case "":
                 errormsg: str = "EOF before tag name"
                 parser_error(self.row, self.col, errormsg)
@@ -213,8 +213,8 @@ class Tokenizer:
             case _:
                 if self.char.isalpha():
                     self._create_token("start tag", name="")
-                    self.state = "tag name"
-                    self.need_to_reconsume = True
+                    self.parser_state.state = "tag name"
+                    self.parser_state.need_to_reconsume = True
                 else:
                     errormsg: str = "Invalid first character of tag-name"
                     parser_error(self.row, self.col, errormsg)
@@ -222,31 +222,31 @@ class Tokenizer:
     def _tag_name_state(self) -> None:
         match self.char:
             case "\t" | "\n" | "\f" | " ":
-                self.state = "before attr name"
+                self.parser_state.state = "before attr name"
             case "/":
-                self.state = "self-closing start tag"
+                self.parser_state.state = "self-closing start tag"
             case ">":
-                self.state = "data"
-                self._emit_token(self.token)
-                self.token.tag_name = None
+                self.parser_state.state = "data"
+                self._emit_token(self.parser_state.token)
+                self.parser_state.token.tag_name = None
             case "":
                 errormsg: str = "EOF in tag"
                 parser_error(self.row, self.col, errormsg)
                 self._emit_token(Token("EOF", ""))
             case _:
                 if self.char.isalpha():
-                    self.token.tag_name += self.char.lower()  # pyright: ignore[reportOperatorIssue]
+                    self.parser_state.token.tag_name += self.char.lower()  # pyright: ignore[reportOperatorIssue]
                 else:
-                    self.token.tag_name += self.char  # pyright: ignore[reportOperatorIssue]
+                    self.parser_state.token.tag_name += self.char  # pyright: ignore[reportOperatorIssue]
 
     def _rcdata_lt_sign_state(self) -> None:
         match self.char:
             case "/":
                 self._create_token("end tag")
-                self.token.tag_name = ""
-                self.temp_buff = ""
-                self.need_to_reconsume = True
-                self.state = "rcdata end tag open"
+                self.parser_state.token.tag_name = ""
+                self.parser_state.temp_buff = ""
+                self.parser_state.need_to_reconsume = True
+                self.parser_state.state = "rcdata end tag open"
             case _:
                 self._emit_token(Token("char", char="<"))
                 self._emit_token(Token("char", char="/"))
@@ -257,24 +257,24 @@ class Tokenizer:
         def anything_else() -> None:
             self._emit_token(Token("char", char="<"))
             self._emit_token(Token("char", char="/"))
-            for char in self.temp_buff[::-1]:
+            for char in self.parser_state.temp_buff[::-1]:
                 self._emit_token(Token("char", char=char))
 
         match self.char:
             case "\t" | "\n" | "\f" | " ":
-                if is_appropriate_end_tag_token(self.token):
-                    self.state = "before attr name"
+                if is_appropriate_end_tag_token(self.parser_state.token):
+                    self.parser_state.state = "before attr name"
                 else:
                     anything_else()
             case "/":
-                if is_appropriate_end_tag_token(self.token):
-                    self.state = "self-closing start tag"
+                if is_appropriate_end_tag_token(self.parser_state.token):
+                    self.parser_state.state = "self-closing start tag"
                 else:
                     anything_else()
             case ">":
-                if is_appropriate_end_tag_token(self.token):
-                    self.state = "data"
-                    self._emit_token(self.token)
+                if is_appropriate_end_tag_token(self.parser_state.token):
+                    self.parser_state.state = "data"
+                    self._emit_token(self.parser_state.token)
                 else:
                     anything_else()
             case _:
