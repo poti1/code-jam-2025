87b460846416d0c450eeacd14efa77dd9b473b5e
Gimpy3887 <gimpy2345@gmail.com>
2025-08-18 12:07:10 -0400

    Add drawing functions to render class

--- static/python/render.py
+++ static/python/render.py
@@ -1,8 +1,25 @@
-from js import HTMLCanvasElement
+import render_types
 
 
 class Renderer:
     """Represent base render class."""
 
-    def __init__(self, canvas: HTMLCanvasElement) -> None:
-        pass
+    def __init__(self, ctx) -> None:  # noqa: ANN001
+        self.ctx = ctx
+
+    def draw_text(self, el: render_types.Element, x: int, y: int):
+        """Render the text onto the canvas element."""
+        if hasattr(el.content, "text"):
+            self.ctx.fillStyle = "black"
+            self.ctx.fillText(el.content.text, x, y)
+
+    def draw_tree(self, elements: list):
+        """Render elements tree wise."""
+        y = 30
+        node = elements.head
+
+        while node:
+            element = node.element
+            self.draw_text(element, 20, y)
+            y += 20  # line spacing
+            node = node.txt
